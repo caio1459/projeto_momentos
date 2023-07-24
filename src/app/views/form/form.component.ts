f3546b569ec97d1e9e90ca557e5b0318023aa5c8
Caio Henrique <caiogrechic50@gmail.com>
2023-07-23 23:39:17 -0300

    Foi concluido a validação dos forms

--- src/app/views/form/form.component.ts
+++ src/app/views/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,4 +9,30 @@ import { Component, Input } from '@angular/core';
 export class FormComponent {
   @Input() btnText!: String;
 
+  momentoForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentoForm = new FormGroup({
+      id: new FormControl(null),
+      titulo: new FormControl(null, [Validators.required]),
+      descricao: new FormControl(null, [Validators.required]),
+      image: new FormControl(null),
+    });
+  }
+
+  get titulo() {
+    return this.momentoForm.get('titulo')!;
+  }
+
+  get descricao() {
+    return this.momentoForm.get('descricao')!;
+  }
+
+  enviar() {
+    if (this.momentoForm.invalid) {
+      return;
+    } else {
+      console.log('Teste');
+    }
+  }
 }
